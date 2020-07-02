ecc1b2b7bd9394058451f3c58ed40ce06627ef62
jlniuhuan <jlniuhuan@outlook.com>
2020-07-02 23:57:29 +0800

    update theme 2.6.6

--- themes/hexo-theme-volantis-2.2.0/scripts/tags/btns.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function postBtns(args, content) {
-  return `<div class="btns ${args.join(' ')}">
-            ${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}
-          </div>`;
-}
-
-hexo.extend.tag.register('btns', postBtns, {ends: true});
